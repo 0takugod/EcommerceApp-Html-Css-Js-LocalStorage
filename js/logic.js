5bc726097b7b7e4c6d5c7d9e2c9dcc6dccd3d6a8
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-08-03 18:34:51 +0300

    Created Test For FILTER function

--- js/logic.js
+++ js/logic.js
@@ -8,8 +8,20 @@ const checkFormData = (name, price, category, image) => {
   return isValid;
 };
 
+// logic For filter Products
+const filterProduct = (nameCategory, products) => {
+  if (nameCategory === "None" || nameCategory === undefined) {
+    return products;
+  } else {
+    return products.filter((product) => {
+      return product.category === nameCategory;
+    });
+  }
+};
+
 if (typeof module !== "undefined") {
   module.exports = {
     checkFormData,
+    filterProduct,
   };
 }
--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const { checkFormData } = require("../js/script");
+const { checkFormData, filterProduct } = require("../js/script");
 
 describe("Test  Check Form Data", () => {
   test("Check Form Data is Valid", () => {
@@ -8,5 +8,58 @@ describe("Test  Check Form Data", () => {
     expect(checkFormData("name", "", "category", "image")).toBeFalsy();
     expect(checkFormData("", "price", "category", "image")).toBeFalsy();
     expect(checkFormData("", "", "", "")).toBeFalsy();
+  });
+});
+
+describe("Filter Function", () => {
+  test("should return array with clothes cateogre products", () => {
+    const nameCategory = "clothing";
+    let products = [
+      {
+        id: 1,
+        name: "first",
+        price: 10.15,
+        category: "clothing",
+        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      },
+      {
+        id: 2,
+        name: "second",
+        price: 20,
+        category: "Games",
+        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      },
+    ];
+    const filteredList = [
+      {
+        id: 1,
+        name: "first",
+        price: 10.15,
+        category: "clothing",
+        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      },
+    ];
+    expect(filterProduct(nameCategory, products)).toEqual(filteredList);
+  });
+
+  test("should return the same array without any change", () => {
+    let nameCategory;
+    let products = [
+      {
+        id: 1,
+        name: "first",
+        price: 10.15,
+        category: "clothing",
+        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      },
+      {
+        id: 2,
+        name: "second",
+        price: 20,
+        category: "Games",
+        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      },
+    ];
+    expect(filterProduct(nameCategory, products)).toEqual(products);
   });
 });
